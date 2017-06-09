ccbb6c1b12b211cc045b92cb98a889e07c4a2d73
Peng-Fei, He <peng-fei.he@hpe.com>
2017-06-09 18:11:45 +0800

    Update validation of input

--- src/js/contants/ConstantsProperty.js
+++ src/js/contants/ConstantsProperty.js
@@ -1,7 +1,7 @@
 /**
  * Created by hepen on 5/26/2017.
  */
-export const BASE_PROPERTY = ['id', 'name', 'style', 'className', 'disabled'];
+export const BASE_PROPERTY = ['name', 'style', 'className', 'disabled'];
 export const INPUT_PROPERTY = ['checked', 'disabled', 'max', 'min', 'maxLength', 'name', 'placeholder', 'readOnly', 'type', 'value', 'onChange', 'onBlur', 'onFocus'].concat(BASE_PROPERTY);
 export const INPUT_PROPERTY_SINGLE = ['checked', 'disabled', 'readOnly'];
 export const FORM_PROPERTY = ['horizontal'].concat(BASE_PROPERTY);
